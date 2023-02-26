3550b9d3017b6136d6d7e93ebe93525ea5ed92ac
MaksimMihalcov <maksiswet@gmail.com>
2023-02-27 00:05:27 +0300

    init

---
+++ Mob3/Mob3/Views/GeneratePage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
+
+namespace Mob3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GeneratePage : ContentPage
+    {
+        public GeneratePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void GenerateQrCode(object sender, EventArgs e)
+        {
+            if (qrText.Text == null || qrText.Text.Length == 0)
+            {
+                await DisplayAlert("Error: ", "Text is empty!", "Ok");
+                return;
+            }
+            var qr = new ZXingBarcodeImageView
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            qr.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
+            qr.BarcodeOptions.Height = 500;
+            qr.BarcodeOptions.Width = 500;
+            qr.BarcodeOptions.Margin = 10;
+            qr.BarcodeValue = qrText.Text;
+            stackL.Children.Add(qr);
+        }
+    }
+}
---
+++ Mob3/Mob3/Views/ScanPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
+
+namespace Mob3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScanPage : ContentPage
+    {
+        public ScanPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ScanQrCode(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushModalAsync(scan);
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () => 
+                {
+                    await Navigation.PopToRootAsync();
+                    var action = await DisplayAlert("Qr code: ", result.Text, "Copy", "Open in Browser");
+                    if (action)
+                        await Clipboard.SetTextAsync(result.Text);
+                    else
+                    {
+                        try
+                        {
+                            await Browser.OpenAsync(result.Text, BrowserLaunchMode.SystemPreferred);
+                        }
+                        catch
+                        {
+                            await DisplayAlert("Error: ", "Incorrect url format!", "Ok");
+                        }
+                    }
+                });
+            };
+        }
+    }
+}
